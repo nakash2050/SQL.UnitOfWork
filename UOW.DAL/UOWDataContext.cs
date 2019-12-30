a921b95d9dfe3e59223358ea8505544c33cfad91
Nakash Sheikh <nakashsheikh@Nakashs-MacBook-Pro.local>
2019-12-29 21:05:44 -0600

    Added Base files

---
+++ UOW.DAL/UOWDataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UOW.DAL
+{
+    public class UOWDataContext : DbContext
+    {
+        public UOWDataContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
