a981a4034d84ee187cbf88d1a10ef7115ed8860f
Nakash Sheikh <nakashsheikh@Nakashs-MBP.attlocal.net>
2020-04-18 21:30:38 -0500

    Added Unit Tests

---
+++ UOW.Tests/EmployeeControllerTests.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using UOW.Entities.DTO;
+using Xunit;
+
+namespace UOW.Tests
+{
+    public class EmployeeControllerTests : IClassFixture<TestFixture>
+    {
+        private readonly HttpClient httpClient;
+
+        public EmployeeControllerTests(TestFixture fixture)
+        {
+            this.httpClient = fixture.Client;
+        }
+
+        [Fact]
+        public async Task GetAllEmployees_ValidRequest_ReturnsAllEmployees()
+        {
+            // Arrange
+            EmployeeDTO[] employees = null;
+            var requestAddress = "/api/employee";
+
+            // Act
+            var response = await httpClient.GetAsync(requestAddress);
+
+            // Assert
+            if (response.IsSuccessStatusCode)
+            {
+                var responseContent = await response.Content.ReadAsStringAsync();
+                employees = JsonConvert.DeserializeObject<EmployeeDTO[]>(responseContent);
+            }
+
+            Assert.True(employees != null && employees.Count() > 0);
+        }
+
+        [Fact]
+        public async Task GetEmployeeById_PassValidEmployeeId_ReturnsEmployee()
+        {
+            // Arrange
+            var employeeId = 1;
+            EmployeeDTO employee = null;
+            var requestAddress = "/api/employee";
+
+            // Act
+            var response = await httpClient.GetAsync(requestAddress + "/" + employeeId);
+
+            // Assert
+            if (response.IsSuccessStatusCode)
+            {
+                var responseContent = await response.Content.ReadAsStringAsync();
+                employee = JsonConvert.DeserializeObject<EmployeeDTO>(responseContent);
+            }
+
+            Assert.True(employee != null);
+        }
+    }
+}
---
+++ UOW.Tests/TestFixture.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace UOW.Tests
+{
+    public class TestFixture : IDisposable
+    {
+        public static string GetProjectPath(string projectRelativePath, Assembly startupAssembly)
+        {
+            var projectName = startupAssembly.GetName().Name;
+
+            var applicationBasePath = AppContext.BaseDirectory;
+
+            var directoryInfo = new DirectoryInfo(applicationBasePath);
+
+            do
+            {
+                directoryInfo = directoryInfo.Parent;
+
+                var projectDirectoryInfo = new DirectoryInfo(Path.Combine(directoryInfo.FullName, projectRelativePath));
+
+                if (projectDirectoryInfo.Exists)
+                    if (new FileInfo(Path.Combine(projectDirectoryInfo.FullName, projectName, $"{projectName}.csproj")).Exists)
+                        return Path.Combine(projectDirectoryInfo.FullName, projectName);
+            }
+            while (directoryInfo.Parent != null);
+
+            throw new Exception($"Project root could not be located using the application root {applicationBasePath}.");
+        }
+
+
+        public TestFixture()
+        {
+            Client = new HttpClient();
+            Client.BaseAddress = new Uri("http://localhost:5000");
+            Client.DefaultRequestHeaders.Accept.Clear();
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public HttpClient Client { get; }
+
+        public void Dispose()
+        {
+            Client.Dispose();
+        }
+
+        protected virtual void InitializeServices(IServiceCollection services)
+        {
+        }
+    }   
+}
